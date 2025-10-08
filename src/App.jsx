84e5ceefe90c1c1326ff20f755b7ec4830e68e08
= <=>
2025-10-08 15:20:12 +0100

    scroll to bottom issie

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import ScrollToTop from './components/ScrollToTop';
-// ... other imports
+
+import Layout from './layout';
+import Profile from './pages/Profile';
+import Conversations from './pages/Conversations';
+import Payout from './pages/Payout';
+import Login from './pages/Login';
+import Register from './components/auth/Register';
+import PrivateRoute from './components/PrivateRoute'; // Import the PrivateRoute component
+import NotFound from './components/NotFound';
+import CGU from './pages/CGU';
+import CGV from './pages/CGV';
+import Privacy from './pages/Privacy';
+import MentionsLegales from './pages/MentionsLegales';
+import AboutUs from './pages/AboutUs';
+import Contact from './pages/Contact';
 
 function App() {
   return (
